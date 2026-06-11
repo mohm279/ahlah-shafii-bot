f5afdca4acd03f5ac284475308db0afebbc5facb
Mohammad <mohammad.madan2005@gmail.com>
2026-06-11 10:07:51 +0300

    make ahlah main and connect chat

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,5 @@
 import { createFileRoute } from "@tanstack/react-router";
-import { useEffect } from "react";
 
 export const Route = createFileRoute("/")({
-  component: HomeRedirect,
+  component: () => null,
 });
-
-function HomeRedirect() {
-  useEffect(() => {
-    window.location.href = "/ahlah/index.html";
-  }, []);
-
-  return null;
-}
